e3ed88197d3d76aa0d27145d110ac5377e981692
janguenot <devnull@localhost>
2004-04-27 00:10:48 +0000

    Allowing Chat Moderator to handle local roles by monkey patching the CPSMenershiptool variable roles_managing_local_roles

---
+++ CPSMemberShipPatch.py
@@ -0,0 +1,35 @@
+# (C) Copyright 2004 Nuxeo SARL <http://nuxeo.com>
+# Author: Julien Anguenot <ja@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+
+__author__ = "Julien Anguenot <ja@nuxeo.com>"
+
+
+from Products.CPSCore.CPSMembershipTool import CPSMembershipTool
+
+#
+# Monkey patch the CPSMemberShipTool.
+# I want the ChatModerators to be able to manage
+# the local roles within the Chat.
+#
+
+roles_managing_local_roles = ('WorkspaceManager',
+                              'SectionManager',
+                              'ChatModerator')
+
+CPSMembershipTool.roles_managing_local_roles = roles_managing_local_roles
