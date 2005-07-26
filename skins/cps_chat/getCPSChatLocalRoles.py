d0dbebd2f5181d8efe1809b3fd5745cfe7a5af16
atchertchian <devnull@localhost>
2005-07-26 00:51:04 +0000

    Replaced incorrect code producing broken urls using virtual hosting

--- skins/cps_chat/getCPSChatLocalRoles.py
+++ skins/cps_chat/getCPSChatLocalRoles.py
@@ -1,23 +1,22 @@
 ##parameters=mtool=None, base_url=None, context_url=None
-##
 # $Id$
-""" get Merged Local Roles filtering non CPS Roles.
+"""get Merged Local Roles filtering non CPS Roles.
 
 Returns a tuple dict_roles, editable_users, cps_roles, local_roles_blocked
-
 """
+# XXX AT: This method has to be updated like it's been done in CPSDefault, and
+# base_url and context_url are not useful anymore
+
+from Products.CMFcore.utils import getToolByName
 
 if mtool is None:
-    mtool = context.portal_membership
-
-if base_url is None:
-    base_url = context.getBaseUrl()
-
-if context_url is None:
-    context_url = context.getContextUrl()
+    mtool = getToolByName(context, 'portal_membership')
 
 # Get the list of Roles from the tool
 dict_roles = mtool.getMergedLocalRolesWithPath(context)
+
+utool = getToolByName(context, 'portal_url')
+rpath = utool.getRpath(context)
 
 # Filter remove special roles
 local_roles_blocked = 0
@@ -27,7 +26,7 @@ for user in dict_roles.keys():
         roles = [r for r in roles if r not in ('Owner', 'Member')]
         if user == 'group:role:Anonymous' and '-' in roles:
             roles = [r for r in roles if r != '-']
-            if base_url+item['url'] == context_url:
+            if item['url'] == rpath:
                 local_roles_blocked = 1
         item['roles'] = roles
 
@@ -40,7 +39,7 @@ for user in dict_roles.keys():
 editable_users = []
 for user in dict_roles.keys():
     for item in dict_roles[user]:
-        if base_url+item['url'] == context_url:
+        if item['url'] == rpath:
             editable_users.append(user)
             continue
 
