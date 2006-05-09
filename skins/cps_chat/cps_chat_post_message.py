f1bc4f10756be26bdd830c7948bfa9f2b8c8c1e5
madarche <devnull@localhost>
2006-05-09 09:55:52 +0000

    - Fixed #1643 : The chat area spits error pages when a user is not sending   messages. - Stopped empty messages to be sent in the chat area since they are useless and   users may also misuse it. So now when a user "Submit" if there isn't any   message in the input area, no empty line will appear in the chat area.

--- skins/cps_chat/cps_chat_post_message.py
+++ skins/cps_chat/cps_chat_post_message.py
@@ -3,9 +3,10 @@
 if REQUEST is not None:
     if getattr(REQUEST, 'form', None):
         form = REQUEST.form
-        message = form['message']
+        message = form.get('message', '')
         pseudo = form.get('pseudo', '')
-        if not pseudo and context.portal_membership.isAnonymousUser():
-            pseudo = 'Anonymous'
-        context.addChatItem(message=message, pseudo=pseudo)
+        if message:
+            if not pseudo and context.portal_membership.isAnonymousUser():
+                pseudo = 'Anonymous'
+            context.addChatItem(message=message, pseudo=pseudo)
 return context.cps_chat_main_frame()
